9609475050d7d5b2acdd5a40eee7f2914a05332c
bruno <bruno.pfsilva@hotmail.com>
2019-07-15 23:56:49 +0100

    ecrã tipo de urgencia

--- app/src/main/java/com/example/cm2019pf/view/tipoUrgenciaActivity.java
+++ app/src/main/java/com/example/cm2019pf/view/tipoUrgenciaActivity.java
@@ -2,7 +2,14 @@ package com.example.cm2019pf.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.cm2019pf.R;
 
@@ -16,5 +23,31 @@ public class tipoUrgenciaActivity extends AppCompatActivity {
         this.setTitle("Tipo Urgencia");
 
 
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+        DialogFragment dialogFragment = new MyCustomDialogFragment();
+        dialogFragment.show(ft, "dialog");
+
+    }
+
+    @SuppressLint("ValidFragment")
+    public class MyCustomDialogFragment extends DialogFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.fragment_my_custom_dialog, container, false);
+
+            // Do all the stuff to initialize your custom view
+
+            return v;
+        }
     }
 }
